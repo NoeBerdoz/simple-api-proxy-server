e8061e88200a979fc548925b7391ae43db3e2e99
NoeBerdoz <berdoznoe@gmail.com>
2024-05-04 16:05:07 +0200

    Add google search api get route

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ const PORT = process.env.PORT || 8000;
 const LLM_API_URL = process.env.LLM_API_URL;
 const LLM_API_KEY = process.env.LLM_API_KEY;
 
+const GOOGLE_SEARCH_API_URL = process.env.GOOGLE_SEARCH_API_URL;
+const GOOGLE_SEARCH_API_KEY = process.env.GOOGLE_SEARCH_API_KEY;
+const GOOGLE_SEARCH_CX = process.env.GOOGLE_SEARCH_CX;
+
 // Enable CORS
 app.use(cors());
 
 // Parse JSON request body
 app.use(express.json());
 
+// Route to handle requests to /api/llm/prompt
 app.post('/api/llm/prompt', (req, res) => {
     console.log('[+] Received request to /api/llm/prompt');
 
@@ -41,8 +46,24 @@ app.post('/api/llm/prompt', (req, res) => {
         })
         .catch(error => {
             console.log('Error occurred');
-            console.error(error);
+            console.error(error); // Send error to client
             res.status(500).send('An error occurred while making the request to the LLM API');
+        });
+});
+
+app.get('/api/search', (req, res) => {
+    const query = req.query.q //  search query parameter
+    const url = `${GOOGLE_SEARCH_API_URL}?key=${GOOGLE_SEARCH_API_KEY}&cx=${GOOGLE_SEARCH_CX}&q=${query}&format=json`;
+    needle('get', url)
+        .then(response => {
+            console.log('[+] Google Search Response received');
+            console.log(url);
+            res.json(response.body) // Send response to client
+        })
+        .catch(error => {
+            console.log('Error occurred');
+            console.error(error); // Send error to client
+            res.status(500).send('An error occurred while making the request to the Google Search API');
         });
 });
 
